5e116a3bf62e9d89ebf96b3276b3f26663007780
leeddwcs <ducle@chidoanh.com>
2024-03-03 21:50:16 +0700

    init project

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  const { name = 'World' } = req.query;
   return res.json({
-    message: `Hello ${name}!`,
-  })
+    message: `Hello ${name}!`
+  });
 }
