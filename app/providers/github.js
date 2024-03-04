35e752df189024ce513d2115f42ac5c89681953e
leeddwcs <ducle@chidoanh.com>
2024-03-04 22:46:53 +0700

    feat: github verify authorization code

--- app/providers/github.js
+++ app/providers/github.js
@@ -5,12 +5,15 @@ module.exports = {
   verify: async (input) => {
     const { authorizationCode } = input;
 
-    const res = await axios.post({
-      url: `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${authorizationCode}`,
-      headers: {
-        accept: 'application/json'
+    const res = await axios.post(
+      `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${authorizationCode}`,
+      undefined,
+      {
+        headers: {
+          accept: 'application/json'
+        }
       }
-    });
+    );
 
     const accessToken = res.data.access_token;
 
