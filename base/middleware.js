1f89f60bb06363d00ae452a14d5cdf1ad9528b7f
leeddwcs <ducltauit@gmail.com>
2024-03-17 02:33:51 +0700

    fix: allow cors

--- base/middleware.js
+++ base/middleware.js
@@ -2,12 +2,37 @@ const RuntimeError = require('./runtime-error');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 const middleware = (requireWalletAddress, handler) => {
   return async (req, res) => {
     try {
       const walletAddress = req.headers['x-wallet-address'];
       const query = req.query;
       const body = req.body;
+
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      );
 
       if (isDevelopment) {
         console.info('=================================================================');
