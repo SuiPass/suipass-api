638bda9d562f27e8534a23eecb6412f24d5e6614
leeddwcs <ducltauit@gmail.com>
2024-03-19 21:29:43 +0700

    init nest project

--- base/runtime-error.js
+++
@@ -1,8 +0,0 @@
-module.exports = class RuntimeError extends Error {
-  constructor({ code, message, data }) {
-    super(message);
-    this.code = code;
-    this.message = message;
-    this.data = data;
-  }
-};
